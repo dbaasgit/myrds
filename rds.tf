bd0890a69b86592f339f0aec4e38bce09b506554
Anurag <75475233+dbaasgit@users.noreply.github.com>
2021-03-06 06:33:32 +0000

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -15,13 +15,3 @@ resource "aws_db_instance" "rds_create" {
   parameter_group_name = "default.mysql5.7"
   vpc_security_group_ids = ["sg-0e7dbd515403acadf"]
 }
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terra-backend-bkt"
-    key            = "global/s3/v3/terraform.tfstate"
-    region         = "us-east-2"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-lock-db-test"
-    encrypt        = true
-  }
