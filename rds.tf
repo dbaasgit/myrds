04a37fecce97accb4d964528b1d49b149a690375
Anurag <75475233+dbaasgit@users.noreply.github.com>
2021-03-06 05:54:22 +0000

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+   region     = "us-east-2"
+}
+
+resource "aws_db_instance" "rds_create" {
+  identifier = "mysql-test-snap"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = ["sg-0e7dbd515403acadf"]
+}
