95ba85e81eb062c340224815a942ca1aa420672d
Anurag <75475233+dbaasgit@users.noreply.github.com>
2021-03-06 05:55:09 +0000

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "rds_create" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   name                 = "mydb"
-  username             = "foo"
+  username             = "admin"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
   vpc_security_group_ids = ["sg-0e7dbd515403acadf"]
